593e192adf373f0ce2ec7b2a6c0d15240a12f968
Benjamin Taylor Huemann <bhuemann@data.cs.purdue.edu>
2017-02-04 18:50:57 -0500

    Added sample query and comments. Removed repetative file

--- src/main/webapp/cs408_app/connect.js
+++ src/main/webapp/cs408_app/connect.js
@@ -1,4 +1,8 @@
 var mysql = require("mysql");
+
+//In order to connect to the database you will need node installed on your machine.
+//Once Node's installed, run command 'npm install' in 'CS408/src/main/webapp/cs408_app' dir.
+//Note: This database can only be accessed through Purdue's network.
 var con = mysql.createConnection({
   host: "mydb.itap.purdue.edu",
   user: "bhuemann",
@@ -14,8 +18,16 @@ con.connect(function(err){
   console.log('Connection established');
 });
 
+con.query('SELECT * FROM users',function(err,rows){
+    if(err) throw err;
+
+    console.log('Data received from Db:\n');
+    console.log(rows);
+});
+
 con.end(function(err) {
   // The connection is terminated gracefully
   // Ensures all previously enqueued queries are still
   // before sending a COM_QUIT packet to the MySQL server.
 });
+
