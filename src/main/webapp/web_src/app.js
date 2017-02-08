3a0a5815cb3662c319015c86720d8474da5d970b
Pedro Del Moral Lopez <pdelmora@purdue.edu>
2017-02-08 12:53:10 -0500

    Fixed room modal error

--- src/main/webapp/web_src/app.js
+++ src/main/webapp/web_src/app.js
@@ -47,7 +47,16 @@ app.controller("navbar", function($scope) {
 
 app.controller("reservation", function($scope) {
    
+     var _slots = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      ];
+      function _init() {
+        $scope.slots = _slots;
+      }
 
+      _init();
     $scope.isCollapsed = true;
     $scope.roomsData  = [
         {
@@ -167,10 +176,11 @@ app.controller("reservation", function($scope) {
     ];
     $scope.roomSelected;
     $scope.openModal = function(event) {
+        console.log($scope.roomsData);
         var id = event.target.id;
         $scope.roomSelected=id;
-        $scope.roomSelected=$scope.roomSelected.substring(0, id.length-1);
-        var num = $scope.roomSelected.substring($scope.roomSelected.length -1);
+        $scope.roomSelected=$scope.roomSelected.substring(4, id.length-1);
+        var num = $scope.roomSelected;
         console.log(num);
         if ($scope.roomsData[num-1].blocked == false) {
             $("#reserve-modal").modal("toggle");
@@ -229,6 +239,80 @@ app.controller("reservation", function($scope) {
 app.controller("reservation-modal", function($scope) {
     
 });
+
+app.directive('timetable', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        slots: '='
+      },
+      templateUrl: 'my-timetable-iso.html',
+      link: function (scope, element, attributes) {
+          var _days = ['Room 00', 'Room 01', 'Room 02'];
+          var _selection = {
+            state: false,
+            day: [0, 0, 0],
+            hour: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+          };
+          
+          function _loop(begin, end, step) {
+            var array = [];
+            
+            for (var i = begin; i <= end; i += step) {
+              array.push(i);
+            }
+            
+            return array;
+          }
+          
+          function _toggle(what, day, hour) {
+            var i = 0;
+            switch (what) {
+              case 'day':
+                _selection.day[day] = !_selection.day[day];
+                
+                for (i = 0; i < 24; i++) {
+                  scope.slots[day][i] = _selection.day[day]; 
+                }
+              break;
+              
+              case 'hour':
+                _selection.hour[hour] = !_selection.hour[hour];
+                
+                for (i = 0; i < 3; i++) {
+                  scope.slots[i][hour] = _selection.hour[hour];
+                }
+              break;
+              
+              case 'slot':
+                if (_selection.state) {
+                  scope.slots[day][hour] = !scope.slots[day][hour];
+                }
+              break;
+            }
+          }
+          
+          function _select(state, day, hour) {
+            _selection.state = state;
+            
+            if (_selection.state) {
+              _toggle('slot', day, hour);
+            }
+          }
+          
+          function _init() {
+            scope.loop = _loop;
+            scope.toggle = _toggle;
+            scope.select = _select;
+            
+            scope.days = _days;
+          }
+          
+          _init();
+        }
+      };
+  });
+
 /*
  * POST HOOKING UP WITH BACKEND
 var resApp = angular.module('resMod', []);
