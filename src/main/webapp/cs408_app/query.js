f09d04acdd109532060e29cb6b1a03c86bfc0822
Benjamin Taylor Huemann <benhuemann@gmail.com>
2017-03-03 19:31:31 -0500

    Fixed issue that allowed callback to be called twice

--- src/main/webapp/cs408_app/query.js
+++ src/main/webapp/cs408_app/query.js
@@ -52,7 +52,7 @@ var getUserHours = function(username,connection,callback) {
 
        if(results.length == 1)
 	   callback(null, {"data":results[0].hours_remain});
-       if(results.length == 0)
+       else if(results.length == 0)
 	   callback(null, {"err":"username doesn't exist"});
        else
 	   callback(new Error("Illegal State: multiple hours results from username " + username));
